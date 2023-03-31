45e779e13116c815bf9f43399cadcb7a2b605d62
delushaw <dwanigasuriya@ocadu.ca>
2023-03-30 23:53:17 -0400

    New sketch added

--- 03.30/sketch.js
+++ 03.30/sketch.js
@@ -1,21 +1,79 @@
-var x =0;
-var speed= 5;
-
-function setup(){
-  createCanvas(600, 400);
+class Ball {
+  constructor(pos, vel, radius, color) {
+    this.pos = pos;
+    this.vel = vel;
+    this.radius = radius;
+    this.color = color;
+  }
+  collide(other) {
+    if (other == this) {
+      return;
+    }
+    let relative = p5.Vector.sub(other.pos, this.pos);
+    let dist = relative.mag() - (this.radius + other.radius);
+    if (dist < 0) {
+      let movement = relative.copy().setMag(abs(dist/2));
+      this.pos.sub(movement);
+      other.pos.add(movement);
+      
+      let thisToOtherNormal = relative.copy().normalize();
+      let approachSpeed = this.vel.dot(thisToOtherNormal) + -other.vel.dot(thisToOtherNormal);
+      let approachVector = thisToOtherNormal.copy().setMag(approachSpeed);
+      this.vel.sub(approachVector);
+      other.vel.add(approachVector);
+    }
+  }
+  move() {
+    this.vel.y += 0.1;
+    this.pos.add(this.vel);
+    if (this.pos.x < this.radius) {
+      this.pos.x = this.radius;
+      this.vel.x = -this.vel.x;
+    }
+    if (this.pos.x > width-this.radius) {
+      this.pos.x = width-this.radius;
+      this.vel.x = -this.vel.x;
+    }
+    if (this.pos.y < this.radius) {
+      this.pos.y = this.radius;
+      this.vel.y = -this.vel.y;
+    }
+    if (this.pos.y > height-this.radius) {
+      this.pos.y = height-this.radius;
+      this.vel.y = -this.vel.y;
+    }
+  }
+  render() {
+    fill(this.color);
+    rect (this.pos.x, this.pos.y, this.radius*2, 80, 5, 500, 300 );
+  }
 }
 
-function draw(){
-  background(205,261,163);
-  stroke(255);
-  strokeWeight(4);
-  fill(251,138,215);
-  ellipse(x,200,105,100);
-  rect(x,100,50,150, 150);
-  
+let balls = [];
 
-  if (x>width){
-    x = x -3;
+function setup() {
+  createCanvas(400, 400);
+  for (i = 0; i < 50; i++) {
+    balls.push(new Ball(
+      createVector(random(width),random(height)),
+      p5.Vector.random2D().mult(random(10)),
+      30,
+      color(random(255),random(255),random(255))
+    ));
   }
-  x = x + speed;
+}
+
+function draw() {
+  background(255,153,204);
+  
+  for(let i = 0; i < balls.length; i++) {
+    for(let j = 0; j < i; j++) {
+      balls[i].collide(balls[j]);
+    }
+  }
+  
+  for(let i = 0; i < balls.length; i++) {
+    balls[i].move();
+    balls[i].render();
+  }
 }
