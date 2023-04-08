8331f4af0febe2c7eb0b57b2ca4d481113266332
delushaw <dwanigasuriya@ocadu.ca>
2023-04-07 23:44:54 -0400

    added new sketch

---
+++ 04.09/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+  noStroke();
+  describe('torus with specular material');
+}
+
+function draw() {
+  background(20);
+  fill(316,174,278)
+  ambientLight(30);
+
+  // add point light to showcase specular material
+  let locX = mouseX - width / 2;
+  let locY = mouseY - height / 2;
+  pointLight(255, 255, 255, locX, locY, 50);
+
+  specularMaterial(90);
+  shininess(50);
+  torus(90, 40, 94, 94);
+  torus(-90, 40, 94, 94);
+  rect(-190, -140, 140, 290, 300);
+  
+
+
+}
